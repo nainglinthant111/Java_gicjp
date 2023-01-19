374c758798b3f960970928f658061b2963b01218
nainglinthant111 <oppositedirection111@gmail.com>
2023-01-19 08:07:41 +0630

    Hello World

---
+++ Helloworld.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Helloworld {
+    public static void main(String[] args){
+        String message="Hello World";
+        boolean isEndtWith= message.endsWith("d");
+        System.out.println(isEndtWith);
+        System.out.println();
+    }
+}
